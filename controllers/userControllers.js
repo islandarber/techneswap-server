e6a2c54f9c581f93ece954c79b241a61a76ce2d2
Christina Vekri <christina.vek@gmail.com>
2024-02-26 13:20:54 +0100

    get for single user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,10 +1,21 @@
 import User from '../models/User.js';
 
-export const getUsers = async (req, res) => {
+export const getUsers = async (req, res) => { //endpoint to get all users
   try {
     const users = await User.find().populate("skills");
     res.json(users);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
+
+export const getUser = async (req, res) => { //endpoint to get a single user
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id).populate("skills");
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
