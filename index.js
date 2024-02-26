d25ab42ec3cceeca2533f21d535857f6f99cc8da
Christina Vekri <christina.vek@gmail.com>
2024-02-26 11:24:23 +0100

    connection established

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import express from "express";
 import "dotenv/config";
+import { connectDb } from "./db/client.js";
 
 const app = express();
+const port = 8000;
+
+const startServer = async() => {
+  await connectDb();
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+};
+
+startServer().catch((error) => console.log(error, 'failed to start server'));
