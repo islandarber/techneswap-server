49a51e2ae3b73592b7fe31b15ec87ddd0bd84d07
Christina Vekri <christina.vek@gmail.com>
2024-02-27 10:37:53 +0100

    skill endpoints work!!

--- controllers/skillControllers.js
+++ controllers/skillControllers.js
@@ -1,9 +1,9 @@
 import Skill from "../models/Skill.js";
 
 export const getSkills = async (req, res) => {
-  const { category } = req.query;
+  const category = req.params.id;
   try {
-    const skills = await Skill.find({ category}).populate('category');
+    const skills = await Skill.find({category}).populate('category');
     res.json(skills);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -13,7 +13,8 @@ export const getSkills = async (req, res) => {
 export const createSkill = async (req, res) => {
   const {name, category} = req.body;
   try {
-    const newSkill = await Skill.create({name, category}).populate('category');
+    const newSkill = await Skill.create({name, category});
+    await newSkill.populate('category');
     res.status(201).json(newSkill);
   } catch (error) {
     res.status(500).json({ message: error.message });
